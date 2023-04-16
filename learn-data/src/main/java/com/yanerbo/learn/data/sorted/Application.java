7bf23aab6013a2f4d1b36a999d592d3444f8c9d0
274818 <jihaibo001@deppon.com>
2023-04-16 23:13:14 +0800

    初始化

--- learn-data/src/main/java/com/yanerbo/learn/data/sorted/Application.java
+++ learn-data/src/main/java/com/yanerbo/learn/data/sorted/Application.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 
 public class Application {
 
+    private final static int[] data = {10, 3, 5, 2, 8, 7, 4, 1, 6, 9};
+
     public static void main(String[] args) {
         sort(new BubbleSort());
     }
 
     private static void sort(Sort sort) {
-        int[] data = {10, 3, 5, 2, 8, 7, 4, 1, 6, 9};
         System.out.println(Arrays.toString(sort.sort(data)));
     }
 }
